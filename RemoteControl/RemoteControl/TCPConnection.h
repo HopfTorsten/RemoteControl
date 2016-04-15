350dcf7598fe05ba85ee0f399cb05031338e921d
Torsten <i13009@hb.dhbw-stuttgart.de>
2016-04-15 14:46:32 +0200

    Update TCPConnection.h

--- RemoteControl/RemoteControl/TCPConnection.h
+++ RemoteControl/RemoteControl/TCPConnection.h
@@ -1,3 +1,17 @@
+/**
+RemoteControl is a client server solution for remote control a PC. Copyright (C) 2016 Torsten Hopf
+
+This program is free software: you can redistribute it and/or modify it under the terms of
+the GNU General Public License as published by the Free Software Foundation, either version
+3 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with this program. 
+If not, see http://www.gnu.org/licenses/.
+ * */
 #pragma once
 #include "Connection.h"
 #include "Messages.h"
@@ -11,7 +25,7 @@ namespace remote {
 		TCPConnection(boost::asio::io_service& service,Server& server);
 		virtual ~TCPConnection();
 
-		// Geerbt über Connection
+		// Geerbt Ã¼ber Connection
 		virtual void receive(void * buffer, size_t length, readHandler handler) override;
 		virtual void send(void * data, size_t length, writeHandler handler) override;
 		virtual void receive() override;
