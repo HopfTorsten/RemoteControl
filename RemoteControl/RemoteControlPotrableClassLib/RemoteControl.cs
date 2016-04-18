0abbd9f722582fee5fa6df2c9679445a456e21f7
Torsten <hopf.torsten@web.de>
2016-04-18 11:46:15 +0200

    RemoteControl declariert

---
+++ RemoteControl/RemoteControlPotrableClassLib/RemoteControl.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoteControlPotrableClassLib
+{
+    class RemoteControl : IRemoteControl
+    {
+        public void CloseConnection()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Connect(string address, ushort port)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoteShutdown()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoteVolumeDown()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoteVolumeUp()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
