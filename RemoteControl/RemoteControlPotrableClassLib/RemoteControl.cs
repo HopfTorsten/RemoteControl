fd8bbf2bd14b8902afb701e7f0e11bf062389d7d
Torsten Hopf <i13009@hb.dhbw-stuttgart.de>
2016-04-18 13:39:42 +0200

    Message Factory Declariert Angefangen Verbindungen zu implementieren

--- RemoteControl/RemoteControlPotrableClassLib/RemoteControl.cs
+++ RemoteControl/RemoteControlPotrableClassLib/RemoteControl.cs
@@ -1,36 +1,69 @@
-﻿using System;
+﻿/*
+RemoteControl is a client server solution for remote control a PC. Copyright (C) 2016 Torsten Hopf
+
+This program is free software: you can redistribute it and/or modify it under the terms of
+the GNU General Public License as published by the Free Software Foundation, either version
+3 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with this program. 
+If not, see http://www.gnu.org/licenses/.
+*/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Sockets.Plugin;
 
 namespace RemoteControlPotrableClassLib
 {
     class RemoteControl : IRemoteControl
     {
+        private TcpSocketClient tcpClient;
+        private bool connected = false;
+
+        private const UInt32 SHUTDOWN_COMMAND = 1;
+        private const UInt32 VOLUME_UP_COMMAND = 2;
+        private const UInt32 VOLUME_DOWN_COMMAND = 3;
+        private const UInt32 TOGGLE_MUTE_COMMAND = 4;
+
         public void CloseConnection()
         {
-            throw new NotImplementedException();
+
+          /* await */ tcpClient.DisconnectAsync();
+            connected = false;
         }
 
         public void Connect(string address, ushort port)
         {
-            throw new NotImplementedException();
+           tcpClient = new TcpSocketClient();
+           /*await*/ tcpClient.ConnectAsync(address,port);
+            connected = true;
         }
 
         public void RemoteShutdown()
         {
-            throw new NotImplementedException();
+            if (!connected)
+                throw new InvalidOperationException("Can not shutdown remote host before connecting!");
+
+
         }
 
         public void RemoteVolumeDown()
         {
-            throw new NotImplementedException();
+            if (!connected)
+                throw new InvalidOperationException("Can not decrease volume on remote host before connecting!");
         }
 
         public void RemoteVolumeUp()
         {
-            throw new NotImplementedException();
+            if (!connected)
+                new InvalidOperationException("Can not increace volume on remote host before connecting!");
         }
     }
 }
