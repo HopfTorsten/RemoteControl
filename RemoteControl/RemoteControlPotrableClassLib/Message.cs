1b9a77399d3d2c8e00dd75267625741668d8fd98
Torsten <hopf.torsten@web.de>
2016-04-18 11:33:34 +0200

    C# Lib angelegt. Interface für RemoteControl definiert Message declariert

---
+++ RemoteControl/RemoteControlPotrableClassLib/Message.cs
@@ -0,0 +1,49 @@
+﻿/*
+RemoteControl is a client server solution for remote control a PC. Copyright (C) 2016 Torsten Hopf
+
+This program is free software: you can redistribute it and/or modify it under the terms of
+the GNU General Public License as published by the Free Software Foundation, either version
+3 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with this program. 
+If not, see http://www.gnu.org/licenses/.
+*/
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoteControlPotrableClassLib
+{
+    class Message
+    {
+        
+        private Header header { get; set; }
+        private Body body { get; set; }
+
+
+        public byte[] GetBytes()
+        {
+            return null;
+        }
+
+        class Body
+        {
+            UInt32 command { get; set; }
+        }
+
+        class Header
+        {
+            Byte appType { get; set; }
+            Byte appVersion { get; set; }
+            UInt64 timestamp { get; set; }
+        }
+    }
+}
